adc3e6ce20e60fc01f08e97c1db9a56a44c0724f
AlexCXK <changing199112@gmail.com>
2022-11-03 00:25:34 +0800

    week1

---
+++ week1/MaxPairwiseProduct.py
@@ -0,0 +1,16 @@
+def max_pairwise_product(numbers):
+    n = len(numbers)
+    max_product = 0
+    for first in range(n):
+        for second in range(first + 1, n):
+            max_product = max(max_product,
+                numbers[first] * numbers[second])
+
+    return max_product
+
+
+if __name__ == '__main__':
+    _ = int(input())
+    print(_)
+    input_numbers = list(map(int, input().split()))
+    print(max_pairwise_product(input_numbers))
