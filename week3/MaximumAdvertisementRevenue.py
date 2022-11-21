b3fdf41f8cb2bf0e417c6dcdc592cd8599159470
AlexCXK <changing199112@gmail.com>
2022-11-22 00:41:57 +0800

    week3

---
+++ week3/MaximumAdvertisementRevenue.py
@@ -0,0 +1,13 @@
+def solve(a, b):
+    a.sort()
+    b.sort()
+    res = sum([i * j for i, j in zip(a, b)])
+    return res
+
+
+if __name__ == '__main__':
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    b = [int(i) for i in input().split()]
+    print(solve(a, b))
+
--- week3/MoneyChange.py
+++ week3/MoneyChange.py
@@ -1,29 +1,13 @@
-def solve(W, arr):
-    arr.sort(reverse=True)
-    value = [e[0] for e in arr]
-    weight = [e[1] for e in arr]
-    if weight[0] >= W:
-        return W * value[0]
-    if sum(weight) <= W:
-        return sum([v * w for v, w in zip(value, weight)])
-    res = 0
-    s = 0
-    for i in range(len(weight)):
-        if s + weight[i] > W:
-            break
-        else:
-            s += weight[i]
-            res += weight[i] * value[i]
-    res += (W - s) * value[i]
+def solve(m):
+    c_10 = m // 10
+    m = m % 10
+    c_5 = m // 5
+    m = m % 5
+    c_1 = m
 
-    return res
+    return c_10 + c_5 + c_1
 
 
 if __name__ == '__main__':
-    n, W = map(int, input().split())
-    arr = []
-    for n0 in range(n):
-        v, w = map(int, input().split())
-        arr.append([v / w, w])
-    print(n,W)
-    print("%0.4f" % solve(W, arr))
+    m = int(input())
+    print(solve(m)) 
